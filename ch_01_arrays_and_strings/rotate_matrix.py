54c67c0f27cf033fbc5adb10db4a6a234dabe68e
İdil Sülo <idilsulo@gmail.com>
2020-08-26 01:23:08 +0200

    Added chapter 01 solutions for: * string_rotation * zero_matrix * rotate_matrix

--- ch_01_arrays_and_strings/rotate_matrix.py
+++ ch_01_arrays_and_strings/rotate_matrix.py
@@ -7,5 +7,24 @@ Rotate Matrix:
 * Can you do this in place?
 """
 
-def rotate_matrix():
+"""
+Numpy to rule them all! 
+Vectorization is the fastest solution.
+"""
+import numpy as np
+
+def rotate_matrix_np(m : np.array) -> np.array:
+	m = np.array(m)
+	return np.rot90(m, 3).tolist()
+
+"""
+Maybe add a non-vectorized version later on
+"""
+def rotate_matrix(m : list) -> list:
 	pass
+
+
+if __name__ == '__main__':
+	print("TEST CASE #1 : %r" % (rotate_matrix_np([[1]])))
+	print("TEST CASE #2 : %r" % (rotate_matrix_np([[1,2],[3,4]])))
+	print("TEST CASE #3 : %r" % (rotate_matrix_np([[1,2,3],[4,5,6],[7,8,9]])))
